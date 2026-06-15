c6015398422106d1ebf380ce24655bc9c9b0eec1
ffbiti <ffbiti@noreply.gitcode.com>
2026-06-15 09:53:54 +0800

    增加静默安装

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1322,7 +1322,7 @@ async fn install_update(app_handle: tauri::AppHandle, file_path: String) -> Resu
     }
 
     std::process::Command::new(path)
-        .arg("/UPDATE")
+        .arg("/S")
         .spawn()
         .map_err(|e| format!("启动安装程序失败: {}", e))?;
 
